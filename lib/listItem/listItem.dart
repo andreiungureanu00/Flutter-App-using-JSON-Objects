3a4cc11e9fab863e342fcc208634d746e60113ff
Andrei Ungureanu <andrei090498@gmail.com>
2020-06-24 16:18:40 +0300

    Initial commit

---
+++ lib/listItem/listItem.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatefulWidget {
+  @override
+  ListItemState createState() => ListItemState();
+}
+
+class ListItemState extends State<ListItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
